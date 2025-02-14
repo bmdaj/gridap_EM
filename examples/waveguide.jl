8af15e51a0b770d2a592b8fe7af4433d10f7888b
Benat <bmdaj13@gmail.com>
2025-02-14 16:12:38 +0100

    code more modular, added tags and bc condition handling

--- examples/waveguide.jl
+++ examples/waveguide.jl
@@ -8,11 +8,13 @@ include("../plotter.jl")
 
 print("Defining model parameters...\n")
 
-λ = 35.0          # Wavelength (arbitrary unit)
-k = 2*π/λ        # Wave number 
-ε₁ = 4.0        # Relative electric permittivity for the material
-ε₀ = 1.0         # Relative electric permittivity for the background
-out = true       # Output the results to a file
+λ = 35.0                           # Wavelength (arbitrary unit)
+k = 2*π/λ                          # Wave number 
+ε₁ = 4.0                           # Relative electric permittivity for the material
+ε₀ = 1.0                           # Relative electric permittivity for the background
+εₛ = [0.5*ε₁, ε₁]                  # List of relative permittivities
+tag_list = ["Design", "Passive"]   # List of tag names
+out = true                         # Output the results to a file
 
 # We import the geometry of the problem from the file `geometry.msh`:
 
@@ -32,12 +34,13 @@ print("Setting up FEA...\n")
 
 order = 1 
 degree = 2
+dirichlet_tags = "None"
 neumann_tags = "Edges"
 source_tags = "Source"
 
-U, V, Ω, dΩ, Γ_n, dΓ_n, Γ_s, dΓ_s = fea_init(model, order, degree, neumann_tags, source_tags) 
+U, V, Ω, dΩ, Γ_n, dΓ_n, Γ_s, dΓ_s = fea_init(model, order, degree, dirichlet_tags, neumann_tags, source_tags) 
 
-ε_tag, τ = set_tags(model, ε₁, ε₀)                                         # We set the permittivity tags for the design and passive regions 
+ε_tag, τ = set_tags(model, εₛ, tag_list, ε₀)                                         # We set the permittivity tags for the design and passive regions 
 
 w(x) = -1.0im * k                                                          # absorbing boundary cpndition coefficient
 a(u,v) = ∫(  (∇(v))⊙(∇(u)) - (k^2*((ε_tag∘τ)*v*u))  )dΩ  + ∫( v*u*w )*dΓ_n # LHS weak form
--- fea.jl
+++ fea.jl
@@ -1,49 +1,71 @@
 using Gridap
 using Gridap.Geometry
 
-function fea_init(model, order, degree, neumann_tags, source_tags)
+function fea_init(model, order, degree, dirichlet_tags="None", neumann_tags="None", source_tags="None")
 
     # Define the FEM space
 
     reffe = ReferenceFE(lagrangian,Float64,order)
-    V = TestFESpace(model,reffe, dirichlet_tags=neumann_tags, vector_type=Vector{ComplexF64})
+
+    if dirichlet_tags == "None"
+        V = TestFESpace(model,reffe, vector_type=Vector{ComplexF64})
+    else
+        V = TestFESpace(model,reffe, dirichlet_tags=dirichlet_tags, vector_type=Vector{ComplexF64})
+    end
+
     U = V # mathematically equivalent to TrialFESpace(V,0)
     Ω = Triangulation(model)
     dΩ = Measure(Ω,degree)
-    Γ_n = BoundaryTriangulation(model;tags=neumann_tags)
-    dΓ_n = Measure(Γ_n,degree)
 
-    if source_tags == "Source"
+    if neumann_tags == "None"
+        
+        Γ_n = false
+        dΓ_n = false
+    
+    else 
+
+        Γ_n = BoundaryTriangulation(model;tags=neumann_tags)
+        dΓ_n = Measure(Γ_n,degree)
+
+    end
+
+   
+    if source_tags == "None"
+
+        Γ_s = false
+        dΓ_s = false
+
+    else   
+
         Γ_s = BoundaryTriangulation(model;tags=source_tags)
         dΓ_s = Measure(Γ_s,degree)
 
-        return U, V, Ω, dΩ, Γ_n, dΓ_n, Γ_s, dΓ_s
-    else
-
-        return U, V, Ω, dΩ, Γ_n, dΓ_n, false, false
+        
     end
-    
+
+    return U, V, Ω, dΩ, Γ_n, dΓ_n, Γ_s, dΓ_s    
+
 end
 
-function set_tags(model, ε₁, ε₀)
+function set_tags(model, εₛ, tag_list, ε₀)
 
     labels = get_face_labeling(model)
     dimension = num_cell_dims(model)
     tags = get_face_tag(labels,dimension)
-    #design_tag = get_tag_from_name(labels,"Design")
-    passive_tag = get_tag_from_name(labels,"Passive")
+    τ = CellField(tags,Ω) 
 
     function ξ(tag)
-        #if tag == design_tag
-        #   return 0.5*ε₁
-        if tag == passive_tag
-            return ε₁
-        else
-            return ε₀
-        end
-    end
 
-    τ = CellField(tags,Ω) 
+        for i in range(1, length(tag_list))
+
+            tag_name = get_tag_from_name(labels,tag_list[i])
+
+            if tag == tag_name
+                return εₛ[i]
+            end            
+        end
+        return ε₀
+    end
 
     return ξ, τ
 
