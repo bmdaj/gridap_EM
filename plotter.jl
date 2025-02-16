2ce31a975f26734390d9b84f5b35dca6f35c7b17
Benat <bmdaj13@gmail.com>
2025-02-16 11:47:59 +0100

    fixed plot aspect ratio

--- plotter.jl
+++ plotter.jl
@@ -2,18 +2,21 @@ using Gridap, GridapMakie, GLMakie
 
 function plot_e_field(uh, Ω)
     fig, ax, plt = GLMakie.plot(Ω, imag(uh))
+    ax.aspect = DataAspect()
     Colorbar(fig[1, 2], plt)
     save("examples/plots/e_field.png", fig)
 end
 
 function plot_e_norm(uh, Ω)
     fig, ax, plt = GLMakie.plot(Ω, real(uh * conj(uh)), colormap=:inferno)
+    ax.aspect = DataAspect()
     Colorbar(fig[1, 2], plt)
     save("examples/plots/e_norm.png", fig)
 end
 
 function plot_perm(ε, Ω)
     fig, ax, plt = GLMakie.plot(Ω, real(ε), colormap=:viridis)
+    ax.aspect = DataAspect()
     Colorbar(fig[1, 2], plt)
     save("examples/plots/perm.png", fig)
 end
